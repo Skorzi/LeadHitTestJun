6974a26199dc7a3610400773347e19b4d9c58cb5
Skorzi <lobarev_danil@mail.ru>
2023-11-23 16:41:00 +0500

    Добавил поиск по базе и возвращение номера столбца

---
+++ db.py
@@ -0,0 +1,36 @@
+from tinydb import TinyDB, Query
+from formchecker import Form
+
+
+def checkQueryDB(form):
+
+    db = TinyDB('tables/db.json')
+    Form_queries = Query()
+    dict_form_orig = {'email': form.email, 'phone': form.phone, 'date': form.date, 'text': form.text}
+
+    dict_form = {}
+    for key, value in dict_form_orig.items():
+        if value is not None:
+            dict_form[key] = value
+
+    for key, value in dict_form.items():
+    # Создаем запрос к таблице для поиска элемента с заданным ключом и значением
+        result = db.search(Form_queries[key] == value)
+        if not result:
+            print(f"Элемент с ключом '{key}' и значением '{value}' не найден в таблице")
+
+    for t in result:
+        for key, value in dict_form.items():
+            if key in t and t[key] == value:
+                pass
+            else:
+                result.remove(t)
+                
+    if len(result) > 0:
+        return result[0].doc_id
+    else:
+        return None
+
+if __name__ == '__main__':
+    form = Form("", '', '', 'q')
+    print(checkQueryDB(form))
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from typing import Union
 from urllib.parse import urlparse, parse_qs
 from fastapi import FastAPI
-from dataclasses import dataclass
-from email_validator import validate_email, EmailNotValidError
+from db import checkQueryDB
 from formchecker import Form
+from tinydb import TinyDB, Query
 
 app = FastAPI()
 
@@ -18,18 +18,20 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.post("/get_form/{query}")
 def return_form_or_field(query):
     query_parse = parse_qs(query)
-    form_for_check = Form()
+    FormCheck = Form()
     
     if 'email' in query_parse:
-        form_for_check.email = query_parse['email'][0]
+        FormCheck.email = query_parse['email'][0]
     
     if 'phone' in query_parse:
-        form_for_check.phone = query_parse['phone'][0]
+        FormCheck.phone = query_parse['phone'][0]
     
     if 'date' in query_parse:
-        form_for_check.date = query_parse['date'][0]
+        FormCheck.date = query_parse['date'][0]
     
     if 'text' in query_parse:
-        form_for_check.text = query_parse['text'][0]
+        FormCheck.text = query_parse['text'][0]
     
-    return {"all": [form_for_check.email, form_for_check.phone, form_for_check.date, form_for_check.text]}
+    num_of_table = checkQueryDB(FormCheck)
+    
+    return {"template name": num_of_table}
